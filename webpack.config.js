5ba1e8e2a77f38ef2379963c49aa65578e7ddd79
Billions Joel <joelatugonza@gmail.com>
2022-02-01 00:38:35 +0300

    Fix linter

--- webpack.config.js
+++ webpack.config.js
@@ -4,47 +4,47 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const path = require('path');
 
 module.exports = {
-	mode: 'development',
-	entry: './src/index.js',
-	output: {
-		filename: 'bundle.js',
-		path: path.resolve(__dirname, './dist'),
-		clean: true,
-	},
-	plugins: [
-		new HtmlWebpackPlugin({
-			template: './src/index.html',
-		}),
-		new MiniCssExtractPlugin({
-			// Options similar to the same options in webpackOptions.output
-			// both options are optional
-			filename: 'modules/assets/css/style.css',
-			chunkFilename: 'modules/assets/css/[id].css',
-		}),
-	],
-	module: {
-		rules: [
-			{
-				test: /\.scss$/i,
-				exclude: /node_modules/,
-				use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
-			},
-			{
-				test: /\.(png|jpeg|jpg|gif)$/i,
-				use: {
-					loader: 'file-loader',
-					options: {
-						name: '[name].[hash:6].[ext]',
-						outputPath: 'images',
-						publicPath: 'images',
-						emitFile: true,
-						esModule: false,
-					},
-				},
-			},
-		],
-	},
-	devServer: {
-		static: './dist',
-	},
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, './dist'),
+    clean: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+    new MiniCssExtractPlugin({
+      // Options similar to the same options in webpackOptions.output
+      // both options are optional
+      filename: 'modules/assets/css/style.css',
+      chunkFilename: 'modules/assets/css/[id].css',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.scss$/i,
+        exclude: /node_modules/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.(png|jpeg|jpg|gif)$/i,
+        use: {
+          loader: 'file-loader',
+          options: {
+            name: '[name].[hash:6].[ext]',
+            outputPath: 'images',
+            publicPath: 'images',
+            emitFile: true,
+            esModule: false,
+          },
+        },
+      },
+    ],
+  },
+  devServer: {
+    static: './dist',
+  },
 };
