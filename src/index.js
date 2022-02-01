abbc5b9d365c81ff8bdbe757b831df1a1d3c5fc3
Billions Joel <joelatugonza@gmail.com>
2022-02-01 19:54:31 +0300

    Use fetch api to test reponse

--- src/index.js
+++ src/index.js
@@ -1,2 +1,5 @@
 //  js entry file
 import './modules/assets/sass/style.scss';
+import getRecipies from './modules/models/api';
+
+getRecipies();
--- src/modules/models/api.js
+++ src/modules/models/api.js
@@ -11,3 +11,21 @@
  | Api Key : l1wyQq7Jrf2t34Tkma8G
  |
  */
+
+//class recipes {
+//	constructor(queryResults) {
+//		this.result = queryResults;
+//	}
+//}
+
+const getRecipies = async () => {
+	const response = await fetch(
+		'https://www.themealdb.com/api/json/v1/1/categories.php'
+	)
+		.then((response) => response.json())
+		.then((data) => data);
+
+	console.log(response);
+};
+
+export default getRecipies;
