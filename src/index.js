4b8c953e9ffa5d79b29c3d7d526f7ab0d1aa4c0e
tresorsawasawa <tresorsawasawa@gmail.com>
2022-02-02 01:44:17 +0200

    Add event to display the comments popup

--- src/index.js
+++ src/index.js
@@ -1,2 +1,11 @@
 //  js entry file
 import './modules/assets/sass/style.scss';
+import { displayPopup } from './modules/models/controllers.js';
+
+const showPopup = document.querySelectorAll('.btn');
+
+showPopup.forEach((btn) => {
+  btn.addEventListener('click', () => {
+    displayPopup();
+  });
+});
