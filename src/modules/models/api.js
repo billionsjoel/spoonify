187f582ed7474026dc8623ab7a5bc0347bbe9191
Billions Joel <joelatugonza@gmail.com>
2022-02-01 09:41:07 +0300

    Generated api key for the involvement API and found external Api for recipe

--- src/modules/models/api.js
+++ src/modules/models/api.js
@@ -3,8 +3,11 @@
  | Api Management
  |--------------------------------------------------------------------------
  |
- | @baseApi => https://www.themealdb.com/api.php
- |    https://www.notion.so/Involvement-API-869e60b5ad104603aa6db59e08150270
- | @involvementApi =>  https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/
+ | @baseApi => www.themealdb.com/api/json/v1/1/categories.php
+ | BaseUrl : https://www.themealdb.com/api.php
+ |
+ | @involvementApi => https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/jOmvR28ksoZ7GUF5P2Cy/likes
+ | BaseUrl : https://www.notion.so/Involvement-API-869e60b5ad104603aa6db59e08150270
+ | Api Key : l1wyQq7Jrf2t34Tkma8G
  |
  */
