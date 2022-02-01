329580a9c3936de08aead69b9541a59292d0ecbd
Billions Joel <joelatugonza@gmail.com>
2022-02-01 20:07:15 +0300

    Use fetch api to test reponse

--- src/modules/models/api.js
+++ src/modules/models/api.js
@@ -21,11 +21,8 @@
 const getRecipies = async () => {
 	const response = await fetch(
 		'https://www.themealdb.com/api/json/v1/1/categories.php'
-	)
-		.then((response) => response.json())
-		.then((data) => data);
-
-	console.log(response);
+	);
+	return response.json();
 };
 
 export default getRecipies;
