b1a3cb444eaccc7feae02fb47ae38064522aa79c
tresorsawasawa <tresorsawasawa@gmail.com>
2022-02-02 13:20:57 +0200

    Update event to display the popup

--- src/index.js
+++ src/index.js
@@ -2,8 +2,17 @@
 import './modules/assets/sass/style.scss';
 import Recipies from './modules/models/api.js';
 import renderResults from './modules/views/renderResults.js';
+import { closePopup, displayPopup} from './modules/models/controllers.js';
 
 const state = {};
+
+const seePop = () => {
+  const commentBnts = document.querySelectorAll('.btn');
+  console.log(commentBnts);
+  commentBnts.forEach((btn) => {
+    btn.addEventListener('click', displayPopup);
+  });
+};
 
 const updateGlobalState = async () => {
   // instatiate api call
@@ -13,10 +22,10 @@ const updateGlobalState = async () => {
 
   // search for results
   await state.recipe.getRecipies();
-
   //  render results on the UI
   // console.log(state.recipe.results);
   renderResults(state.recipe.results.categories);
+  seePop();
 };
 
 window.addEventListener('load', () => {
