a344342581afbfbd163246623f9466bf8d555319
tresorsawasawa <tresorsawasawa@gmail.com>
2022-02-02 13:30:47 +0200

    Update event to to close the popup

--- src/index.js
+++ src/index.js
@@ -2,17 +2,19 @@
 import './modules/assets/sass/style.scss';
 import Recipies from './modules/models/api.js';
 import renderResults from './modules/views/renderResults.js';
-import { closePopup, displayPopup} from './modules/models/controllers.js';
+import { closePopup, displayPopup } from './modules/models/controllers.js';
 
 const state = {};
+const hidePopup = document.querySelector('.close-btn');
 
 const seePop = () => {
   const commentBnts = document.querySelectorAll('.btn');
-  console.log(commentBnts);
   commentBnts.forEach((btn) => {
     btn.addEventListener('click', displayPopup);
   });
 };
+
+hidePopup.addEventListener('click', closePopup);
 
 const updateGlobalState = async () => {
   // instatiate api call
