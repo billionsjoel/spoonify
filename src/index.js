c983298f887ca968f1d8b7e93341102d5717720f
Billions Joel <joelatugonza@gmail.com>
2022-02-02 13:22:51 +0300

    Add render loader function

--- src/index.js
+++ src/index.js
@@ -2,23 +2,29 @@
 import './modules/assets/sass/style.scss';
 import Recipies from './modules/models/api.js';
 import renderResults from './modules/views/renderResults.js';
+import {
+	renderLoader,
+	cardsContainer,
+	clearLoader,
+} from './modules/views/UI.js';
 
 const state = {};
 
 const updateGlobalState = async () => {
-  // instatiate api call
-  state.recipe = new Recipies();
+	// instatiate api call
+	state.recipe = new Recipies();
 
-  // prepare UI for results
+	// prepare UI for results
+	renderLoader(cardsContainer);
+	// search for results
+	await state.recipe.getRecipies();
 
-  // search for results
-  await state.recipe.getRecipies();
-
-  //  render results on the UI
-  // console.log(state.recipe.results);
-  renderResults(state.recipe.results.categories);
+	//  render results on the UI
+	// console.log(state.recipe.results);
+	clearLoader();
+	renderResults(state.recipe.results.categories);
 };
 
 window.addEventListener('load', () => {
-  updateGlobalState();
+	updateGlobalState();
 });
