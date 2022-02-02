385777da857bfda430300d51b0684ef34bfd9c7e
Billions Joel <joelatugonza@gmail.com>
2022-02-02 18:01:12 +0300

    Add pagination buttons dynamically

--- src/modules/views/renderResults.js
+++ src/modules/views/renderResults.js
@@ -20,6 +20,36 @@ const renderRecipe = (recipe) => {
 	cardsContainer.innerHTML += cardHtml;
 };
 
+const createBtn = (page, type) => {
+	return `<button class="pagination" data-goto = ${
+		type === 'prev' ? page - 1 : page + 1
+	} >
+    <span>Page ${type === 'prev' ? page - 1 : page + 1}</span>
+${
+	type === 'prev'
+		? `<svg xmlns="http://www.w3.org/2000/svg" height="15" width="20" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 19l-7-7 7-7m8 14l-7-7 7-7" />
+</svg>`
+		: `<svg
+			xmlns="http://www.w3.org/2000/svg"
+			height="15"
+			width="20"
+			class="right"
+			fill="none"
+			viewBox="0 0 24 24"
+			stroke="currentColor"
+		>
+			<path
+				stroke-linecap="round"
+				stroke-linejoin="round"
+				stroke-width="2"
+				d="M13 5l7 7-7 7M5 5l7 7-7 7"
+			/>
+		</svg>`
+}
+
+      </button>`;
+};
 
 const renderBtns = (page, numResults, resPerPage) => {
 	const pages = Math.ceil(numResults / resPerPage);
