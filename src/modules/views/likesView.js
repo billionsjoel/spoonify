e92b8ec32605ec23d8377a0e0bebd2ebbe03ac52
Billions Joel <joelatugonza@gmail.com>
2022-02-03 22:21:18 +0300

    Remove unused imports

--- src/modules/views/likesView.js
+++ src/modules/views/likesView.js
@@ -1,5 +1,3 @@
-import { cardsContainer, paginationContainer, likesContainer } from './UI.js';
-
 export const toggleLikeBtn = (isLiked, idCategory) => {
 	const iconString = isLiked
 		? `<svg xmlns="http://www.w3.org/2000/svg" height="30" width="30" class="h-6 w-6 text-primary" fill="none" viewBox="0 0 24 24" stroke="currentColor">
