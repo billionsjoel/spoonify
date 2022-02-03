c224db141b2ee6ac6ac12b5c78a6284515b92055
Billions Joel <joelatugonza@gmail.com>
2022-02-03 22:17:02 +0300

    Remove console.log debug

--- src/modules/views/likesView.js
+++ src/modules/views/likesView.js
@@ -1,7 +1,6 @@
 import { cardsContainer, paginationContainer, likesContainer } from './UI.js';
 
 export const toggleLikeBtn = (isLiked, idCategory) => {
-	console.log(isLiked);
 	const iconString = isLiked
 		? `<svg xmlns="http://www.w3.org/2000/svg" height="30" width="30" class="h-6 w-6 text-primary" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                   <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
