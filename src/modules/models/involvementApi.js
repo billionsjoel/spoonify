831f3db7b9d7e5611a3ab5c923bd61f5f8fe8e47
Billions Joel <joelatugonza@gmail.com>
2022-02-04 13:50:28 +0300

    Fix merge conflict

--- src/modules/models/involvementApi.js
+++ src/modules/models/involvementApi.js
@@ -1,14 +1,13 @@
 const url = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/l1wyQq7Jrf2t34Tkma8G/comments';
 
 export const sendComment = async (newData) => {
-  const response = await fetch(`${url}`,
-    {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(newData),
-    });
+  const response = await fetch(`${url}`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(newData),
+  });
   return response.text();
 };
 
