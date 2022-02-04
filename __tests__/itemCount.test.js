9c694fee594d33c7d02c6d950bf9500def6c0b06
Billions Joel <joelatugonza@gmail.com>
2022-02-04 10:43:44 +0300

    Check if count is correct

--- __tests__/itemCount.test.js
+++ __tests__/itemCount.test.js
@@ -1,6 +1,8 @@
 document.body.innerHTML = '<ul class="nav-items"> </ul>';
 const { getItemsCount } = require('../src/modules/views/renderResults.js');
 
-
+test('Check if count is correct', () => {
+	expect(getItemsCount(14)).toBe(14);
+});
 
 
