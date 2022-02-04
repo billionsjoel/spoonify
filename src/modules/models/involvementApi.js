3fd966dc533a0e08528508a25dba7eac76ec59c0
tresorsawasawa <tresorsawasawa@gmail.com>
2022-02-04 02:06:12 +0200

    Add feature to get comment data from the API for a selected recipe

--- src/modules/models/involvementApi.js
+++ src/modules/models/involvementApi.js
@@ -11,3 +11,10 @@ export const sendComment = async (newData) => {
     });
   return response.text();
 };
+
+const URL = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/l1wyQq7Jrf2t34Tkma8G/comments?item_id=';
+export const getComments = async (id) => {
+  const res = await fetch(`${URL}${id}`);
+  const comments = await res.text();
+  return comments;
+};
