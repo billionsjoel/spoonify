6cb403fb510f8d262ab8454ebd3b57b7541d0277
Billions Joel <joelatugonza@gmail.com>
2022-02-04 10:44:37 +0300

    Fix Eslinter

--- __tests__/itemCount.test.js
+++ __tests__/itemCount.test.js
@@ -2,9 +2,9 @@ document.body.innerHTML = '<ul class="nav-items"> </ul>';
 const { getItemsCount } = require('../src/modules/views/renderResults.js');
 
 test('Check if count is correct', () => {
-	expect(getItemsCount(14)).toBe(14);
+  expect(getItemsCount(14)).toBe(14);
 });
 
 test('Check li returned', () => {
-	expect(document.querySelectorAll('.nav-items li').length).toBe(3);
+  expect(document.querySelectorAll('.nav-items li').length).toBe(3);
 });
