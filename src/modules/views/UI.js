3b96e75a8e3afcc1b2a2f0c9c26e708778410678
Billions Joel <joelatugonza@gmail.com>
2022-02-04 09:13:16 +0300

    Grab list items container

--- src/modules/views/UI.js
+++ src/modules/views/UI.js
@@ -1,6 +1,7 @@
 const cardsContainer = document.querySelector('.cards');
 const paginationContainer = document.querySelector('.pagination-container');
 const likesContainer = document.querySelector('.likes');
+const navListContainer = document.querySelector('.nav-items');
 
 const clearResults = () => {
   cardsContainer.innerHTML = '';
